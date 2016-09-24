074a8c5ab138481f41ca41112b7bc16fd02e7383
William Enders <endew231@newschool.edu>
2016-09-24 12:18:33 -0400

    scale

---
+++ Assets/moveCamera.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCamera : MonoBehaviour {
+
+	private Vector3 move;
+	public float speed;
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationX = 0F;
+	float rotationY = 0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		move = new Vector3 (Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),Input.GetAxis("Forward"));
+		move *= speed;
+
+		transform.position += ((transform.right * move.x) + (transform.up * move.y) + (transform.forward * move.z)) * Time.deltaTime;
+
+		RightClick ();
+
+	}
+
+	void RightClick ()
+	{
+		if (Input.GetMouseButton(1)) {
+			if (axes == RotationAxes.MouseXAndY) {
+				rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityX;
+				rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
+			} else if (axes == RotationAxes.MouseX) {
+				transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityX, 0);
+			} else {
+				rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				transform.localEulerAngles = new Vector3 (-rotationY, transform.localEulerAngles.y, 0);
+			}
+		}
+	}
+}
---
+++ Assets/reSizeObject.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reSizeObject : MonoBehaviour {
+	public Transform cam;
+	private Vector3 mousePos;
+
+	public Transform obj;
+	private Vector3 objSize,objPos,newSize,deltaSize;
+
+	public Transform[] handle;
+	private Vector3 handlePos, screenHandlePos, newHandlePos;
+
+	public int axis;
+	public float sizeDamp;
+	public int flip;
+
+	public bool drag;
+
+	// Use this for initialization
+	void Start () {
+		drag = false;
+		objSize = obj.localScale;
+
+	}
+	
+	// Update is called once per frame
+
+	void FixedUpdate(){
+		UpdateLinePos ();
+		UpdateGizmoPos ();
+	}
+
+	void Update () {
+
+
+
+		mousePos = Input.mousePosition;
+		
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit)) {
+				for (int i = 0; i < handle.Length; i++) {
+					if (hit.transform == handle [i]) {
+						
+						axis = i;
+						drag = true;
+
+						handlePos = handle [axis].localPosition;
+						screenHandlePos = Camera.main.WorldToScreenPoint(handle [axis].position);
+						objPos = Camera.main.WorldToScreenPoint(obj.position);
+
+						if (axis == 0 || axis == 1) {
+							flip = screenHandlePos [axis] < objPos [axis] ? -1 : 1;
+						} else {
+							flip = screenHandlePos [0] < objPos [0] ? -1 : 1;
+						}
+
+						objSize = obj.localScale;
+					}
+				}
+			}
+		}
+		if (drag) {
+			if (Input.GetMouseButton (0)) {
+				
+					UpdateSize ();
+					newHandlePos = handle [axis].localPosition;
+					if (axis == 0 || axis == 1) {
+						newHandlePos [axis] = 1 - deltaSize [axis];
+					} else {
+						newHandlePos [axis] = 1 - deltaSize [0];
+					}
+					UpdateHandlePos (newHandlePos);
+
+			}
+
+			if (Input.GetMouseButtonUp (0)) {
+					UpdateHandlePos (handlePos);
+				drag = false;
+			}
+		}
+	}
+
+	void UpdateHandlePos(Vector3 updatePos){
+		handle [axis].localPosition = updatePos;
+	}
+
+	void UpdateLinePos(){
+		for (int i = 0; i < handle.Length; i++) {
+			handle [i].GetComponent<LineRenderer> ().SetPosition (0,transform.root.transform.position);
+			handle [i].GetComponent<LineRenderer> ().SetPosition (1,handle[i].position);
+		}
+	}
+
+	void UpdateGizmoPos(){
+		Vector3 half = Vector3.Lerp (cam.position, obj.position, 0.5f);
+		transform.position = cam.position - Vector3.Normalize(half) * 4;
+	}
+
+	void UpdateSize(){
+		newSize = obj.localScale;
+		deltaSize = (screenHandlePos - mousePos) / sizeDamp * flip;
+		Vector3 newDeltaSize = deltaSize * Vector3.Distance (cam.position,obj.position);
+		if (axis == 0 || axis == 1) {
+			newSize [axis] = objSize [axis] - newDeltaSize [axis];
+		} else {
+			newSize [axis] = objSize [axis] - newDeltaSize [0];
+		}
+		obj.localScale = newSize;
+	}
+}
